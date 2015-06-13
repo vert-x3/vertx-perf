d1b5b819e5a42aeea67f3c6104fbfbec4c50b572
purplefox <timvolpe@gmail.com>
2015-06-13 16:06:10 +0100

    Added host/port config

--- src/main/java/io/vertx/perf/core/http/SimpleServer.java
+++ src/main/java/io/vertx/perf/core/http/SimpleServer.java
@@ -10,8 +10,12 @@ public class SimpleServer extends AbstractVerticle {
 
   public void start() {
     HttpServer server = vertx.createHttpServer();
+    String host = System.getProperty("vertx.host", "localhost");
+    int port = Integer.getInteger("vertx.port", 8080);
+    System.out.println("Host: " + host);
+    System.out.println("Port: " + port);
     server.requestHandler(req -> {
       req.response().end();
-    }).listen(8080);
+    }).listen(port, host);
   }
 }
