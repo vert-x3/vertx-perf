6089d68f1b2f5de6df1b9c28a539c597d7231f56
Julien Viet <julien@julienviet.com>
2016-05-09 14:59:00 +0200

    Report errors on console

--- src/main/java/io/vertx/perf/core/http/SimpleServer.java
+++ src/main/java/io/vertx/perf/core/http/SimpleServer.java
@@ -1,6 +1,7 @@
 package io.vertx.perf.core.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServer;
@@ -9,6 +10,10 @@ import io.vertx.core.http.HttpServerResponse;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -44,10 +49,21 @@ public class SimpleServer extends AbstractVerticle {
     int port = Integer.getInteger("vertx.port", 8080);
     System.out.println("Host: " + host);
     System.out.println("Port: " + port);
+    Handler<Throwable> errHandler = this::handleErr;
     server.requestHandler(req -> {
+      req.exceptionHandler(errHandler);
       HttpServerResponse resp = req.response();
       setHeaders(resp);
+      resp.exceptionHandler(errHandler);
       resp.end(helloWorldBuffer);
     }).listen(port, host);
+  }
+
+  private final Executor errs = Executors.newSingleThreadExecutor();
+
+  private void handleErr(Throwable t) {
+    errs.execute(() -> {
+      t.printStackTrace();
+    });
   }
 }
